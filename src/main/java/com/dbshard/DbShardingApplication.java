ff58c0df97638376a1827101d6f1526d5019ad6f
chenshuai <chenshuai5@asiainfo.com>
2020-07-08 18:34:30 +0800

    项目创建

---
+++ src/main/java/com/dbshard/DbShardingApplication.java
@@ -0,0 +1,13 @@
+package com.dbshard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbShardingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbShardingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dbshard/DbShardingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dbshard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbShardingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
